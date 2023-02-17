043b415733c24eebbad867f26d5a93b09cf6daca
eeeedgar <oakmillennial@gmail.com>
2023-02-17 22:14:13 +0300

    feat: create review page

---
+++ js/getReviews.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    showReviews();
+});
+
+
+async function showReviews() {
+    let reviews = await getReviews();
+    console.log(reviews);
+
+    for (let i = 0; i < reviews.length; i++) {
+        createReview(reviews[i]);
+    }
+
+    // document.getElementsByClassName('reviews')[0];
+}
+
+function getReviews() {
+    return fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+        .then((response) => response.json())
+        .then((json) => json);
+}
+
+function createReview(data) {
+    let reviews = document.getElementsByClassName('reviews')[0];
+
+    let review = document.createElement('div');
+    review.classList.add('review');
+
+    let username = document.createElement('div');
+    username.classList.add('review__username');
+    username.appendChild(document.createTextNode(data['name']))
+
+    let text = document.createElement('div');
+    text.classList.add('review__text');
+    text.appendChild(document.createTextNode(data['body']))
+
+    review.appendChild(username);
+    review.appendChild(text);
+
+    reviews.appendChild(review);
+}
