9953c9eac927fe69e9481fb2c9543314817f47b5
eeeedgar <oakmillennial@gmail.com>
2023-02-17 21:21:00 +0300

    feat: add active menu item highlight

--- js/loadTime.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    const startTime = new Date().getTime();
-    document.addEventListener('DOMContentLoaded', () => {
-        const stampParagraph = document.getElementById("load-time");
-        window.onload = () => {
-            stampParagraph.innerHTML += `Время загрузки страницы: ${(new Date().getTime() - startTime) / 1000} с`;
-        };
-    });
-})();
